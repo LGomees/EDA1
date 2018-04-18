a1b0c209e3222af80d7fb03646df55941bc7bb91
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-04-18 16:19:04 -0300

    Adicionado função de procurar valor minimo e maximo de um vetor e normalizar um vetor

--- projeto2/projeto2.c
+++ projeto2/projeto2.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-void alocaVectorInt(int, int*);
-float distanciaEuclidiana(int, int*, int*);
+void alocaVectorInt(int, int*); // tamanho do vetor, vetor a ser alocado
+float distanciaEuclidiana(int, int*, int*); // tamanho dos vetores, primeiro vetor, segundo vetor
+int valorMinimo(int, int*); // tamanho do vetor, vetor
+int valorMaximo(int, int*); // tamanho do vetor, vetor
+int *normalizar(int, int*); // retorna o vetor normalizado e como entrada tamanho do vetor e vetor a ser normalizado
 
 int main () {
     int *p, *q;
@@ -55,5 +58,45 @@ float distanciaEuclidiana(int n, int* p, int* q) {
         result += pow((*(p+i) - *(q+i)), 2);
     }
     return sqrt(result);
+}
+
+int valorMinimo(int n, int* x){
+    int min;
+    for(int i=0; i<n; i++){
+        if(i==0){
+            min = *(x+i);
+        }
+        else if(*(x+i) < min){
+            min = *(x+i);
+        }
+    }
+}
+
+int valorMaximo(int n, int* x){
+    int max;
+    for(int i=0; i<n; i++){
+        if(i==0){
+            max = *(x+i);
+        }
+        else if(*(x+i) > max){
+            max = *(x+i);
+        }
+    }
+}
+
+int *normalizar(int n, int* x){
+    int *result, min, max;
+    min = valorMinimo(n, x);
+    max = valorMaximo(n, x);
+    result = (int*) calloc(n, sizeof(int));
+    if(result == NULL){
+        printf("Alocacao falhou. Finalizado. \n");
+        exit(1);
+    }
+
+    for(int i=0; i<n; i++){
+        *(result+i) = (*(x+i) - min) / (max - min);
+    }
+    return result;
 }
 
