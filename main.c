43781912a6b45dd3f48d98c9e8d0acf2d78c9ec0
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-05-10 16:50:16 -0300

    Added contatos.txt and main.c with a list for the contacts, and a function to create a empty list and insert a contact

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pessoa {
+    char *nome;
+    char *telefone;
+    char *endereco;
+    unsigned int cep;
+    char *dataDeNascimento;
+    struct Pessoa *prox;
+} Pessoa;
+
+Pessoa *criaListaVazia();
+Pessoa *insereInicio(Pessoa *l, char*, char*, char*, unsigned int, char*);
+
+int main () {
+
+    return 0;
+}
+
+Pessoa *criaListaVazia() {
+    return NULL;
+}
+
+Pessoa *insereInicio(Pessoa *l, char *nome, char *telefone, char *endereco, unsigned int cep, char *nascimento) {
+
+    Pessoa *novo;
+    novo = (Pessoa *) malloc(sizeof(Pessoa));
+    if(novo == NULL) {
+        printf("Erro na alocação!");
+        exit(-1);
+    }
+
+    novo->nome = nome;
+    novo->telefone = telefone;
+    novo->endereco = endereco;
+    novo->cep = cep;
+    novo->dataDeNascimento = nascimento;
+
+    if(l == NULL) {
+        novo->prox = NULL;
+        return novo;
+    } else {
+        novo->prox = l;
+        return novo;
+    }
+}
