474f05a52b2f8d7e0f26d58689599ba8d3b111ec
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-05-10 16:54:15 -0300

    Added 'libera' function, to free the list

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ typedef struct pessoa {
 
 Pessoa *criaListaVazia();
 Pessoa *insereInicio(Pessoa *l, char*, char*, char*, unsigned int, char*);
+void libera(Pessoa *l);
 
 int main () {
 
@@ -43,5 +44,14 @@ Pessoa *insereInicio(Pessoa *l, char *nome, char *telefone, char *endereco, unsi
     } else {
         novo->prox = l;
         return novo;
+    }
+}
+
+void libera(Pessoa *l) {
+    Pessoa *aux;
+
+    for(aux = l; aux != NULL; l = aux) {
+        aux = aux->prox;
+        free(l);
     }
 }
